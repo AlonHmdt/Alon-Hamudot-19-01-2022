58a77ebf6da9f0324b297f795ef72afeb859a195
Alon Hamudot <alon@smarti.co.il>
2022-01-16 17:13:06 +0200

    to

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import {AppComponent} from './app.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from "@angular/common/http";
-import {StoreModule} from "@ngrx/store";
-import {cityForecastReducer} from "./reducers/city-forecast.reducer";
+import {HttpClientModule} from '@angular/common/http';
+import {StoreModule} from '@ngrx/store';
+import {cityForecastReducer} from './reducers/city-forecast.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {CityForecastEffects} from './effects/city-forecast.effects';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import {CityForecastEffects} from './effects/city-forecast.effects';
     BrowserAnimationsModule,
     HttpClientModule,
     StoreModule.forRoot({cityForecast: cityForecastReducer}),
-    EffectsModule.forRoot([CityForecastEffects])
+    EffectsModule.forRoot([CityForecastEffects]),
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
